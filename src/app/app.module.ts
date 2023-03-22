1f29676583657d88a1e6fdca77c70ca7d2cd2ad1
JpAmori <joao.pedro.am.profissional@gmail.com>
2023-03-22 17:58:00 -0300

    Pokedex-Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Modulos criados por mim
+import { PagesModule } from './pages/pages.module';
+
+
+
+// Componentes criados por mim
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Componentes criados por mim
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.module';
+import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent,
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
